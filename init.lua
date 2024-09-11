33a53f13a9209ef2d7deab063873e0790d4595cd
freddycz <jurinek.stodulka@gmail.com>
2024-09-11 20:34:59 +0200

    added binding for moving multiple lines

--- init.lua
+++ init.lua
@@ -192,6 +192,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv", { desc = 'Move lines down' })
+vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv", { desc = 'Move lines up' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
