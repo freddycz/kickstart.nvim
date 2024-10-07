3d21c960bd98379f3acb5acf9eef372b0d2ee5b2
freddycz <jurinek.stodulka@gmail.com>
2024-10-07 16:03:50 +0200

    css lsp added

--- init.lua
+++ init.lua
@@ -590,6 +590,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         html = {},
+        cssls = {},
         pylsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
